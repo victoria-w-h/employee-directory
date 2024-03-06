630c50d06e825eb4fa7e876748086287099cd0cd
victoria-w-h <victoria@stormcreative.co.uk>
2024-03-06 17:02:07 +0000

    Popup modal

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ fetch(urlAPI)
 	.catch((err) => console.log(err));
 
 // EMPLOYEE DATA
-
 // employeeData is an object that is passed to the displayEmployees function and renamed as employees
 function displayEmployees(employeeData) {
 	employees = employeeData;
@@ -42,3 +41,98 @@ function displayEmployees(employeeData) {
 	});
 	gridContainer.innerHTML = employeeHTML;
 }
+
+// POPUP MODAL
+let currentIndex;
+
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
+function displayModal(index) {
+	let {
+		name,
+		dob,
+		phone,
+		email,
+		location: { city, street, state, postcode },
+		picture,
+	} = employees[index];
+	let streetNumber = JSON.stringify(street.number);
+	let streetName = JSON.stringify(street.name).replace(/['"]+/g, "");
+	let date = new Date(dob.date);
+
+	const modalHTML = `	
+	<div class="modal-copy" data-index=${index}>
+<img class="avatar modal-avatar" src="${picture.large}" />
+<div class="text-container">
+<h2 class="name">${name.first} ${name.last}</h2>
+<p class="email">${email}</p>
+<p class="address">${city}</p>
+<hr />
+<p>${phone}</p>
+<p class="address">${streetNumber} ${streetName}, ${state} ${postcode}</p>
+<p>Birthday:
+${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+</div>
+</div>
+`;
+	overlay.classList.remove("hidden");
+	modalContainer.innerHTML = modalHTML;
+}
+
+modalClose.addEventListener("click", () => {
+	overlay.classList.add("hidden");
+});
+
+gridContainer.addEventListener("click", (e) => {
+	if (e.target !== gridContainer) {
+		clicked = true;
+		const card = e.target.closest(".card");
+		const index = card.getAttribute("data-index");
+		displayModal(index);
+		currentIndex = index;
+	}
+});
+window.onclick = function (event) {
+	if (event.target == overlay) {
+		overlay.classList.add("hidden");
+	}
+};
+
+// next and back functionality
+const nextBtn = document.querySelector(".next-btn");
+const backBtn = document.querySelector(".back-btn");
+
+function nextFunction() {
+	let modalCopy = modalContainer.children[0];
+	let modalIndex = modalCopy.getAttribute("data-index");
+	let numberModelIndex = Number(modalIndex);
+
+	displayModal(numberModelIndex + 1);
+}
+function backFunction() {
+	let modalCopy = modalContainer.children[0];
+	let modalIndex = modalCopy.getAttribute("data-index");
+	let numberModelIndex = Number(modalIndex);
+	displayModal(numberModelIndex - 1);
+}
+
+// to change modal index with left and right keys
+document.onkeydown = function (e) {
+	switch (e.key) {
+		case "ArrowLeft":
+			if (!overlay.classList.contains("hidden")) {
+				backFunction();
+			}
+			break;
+		case "ArrowRight":
+			if (!overlay.classList.contains("hidden")) {
+				nextFunction();
+			}
+	}
+};
+
+const searchbarInput = document.querySelector(".searchbar-container input");
+let cards = gridContainer.children;
+
+searchbarInput.addEventListener("keydown", function () {});
--- overlay.js
+++ overlay.js
@@ -1,50 +1,11 @@
-// const overlay = document.querySelector(".overlay");
-// const modalContainer = document.querySelector(".modal-content");
-// const modalClose = document.querySelector(".modal-close");
-// function displayModal(index) {
-// 	let {
-// 		name,
-// 		dob,
-// 		phone,
-// 		email,
-// 		location: { city, street, state, postcode },
-// 		picture,
-// 	} = employees[index];
-// 	let date = new Date(dob.date);
-// 	const modalHTML = `
-// <img class="avatar" src="${picture.large}" />
-// <div class="text-container">
-// <h2 class="name">${name.first} ${name.last}</h2>
-// <p class="email">${email}</p>
-// <p class="address">${city}</p>
-// <hr />
-// <p>${phone}</p>
-// <p class="address">${street}, ${state} ${postcode}</p>
-// <p>Birthday:
-// ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
-// </div>
-// `;
-// 	overlay.classList.remove("hidden");
-// 	modalContainer.innerHTML = modalHTML;
-// }
-// modalClose.addEventListener("click", () => {
-// 	overlay.classList.add("hidden");
-// });
+const nextBtn = document.querySelector(".next-btn");
+const backBtn = document.querySelector(".back-btn");
 
-// gridContainer.addEventListener("click", (e) => {
-// 	if (e.target !== gridContainer) {
-// 		clicked = true;
-// 		const card = e.target.closest(".card");
-// 		const index = card.getAttribute("data-index");
-// 		displayModal(index);
-// 	}
-// });
-
-// HTML
-
-// <div class="overlay">
-// 	<div class="modal">
-// 		<button class="modal-close">X</button>
-// 		<div class="modal-content"></div>
-// 	</div>
-// </div>
+nextBtn.addEventListener("click", (e) => {
+	if (clicked) {
+		clicked = false;
+		const card = e.target.closest(".card");
+		const index = card.getAttribute("data-index");
+		displayModal(index);
+	}
+});
